8aa3c53359b56301a91be0973ef636836bc22ac0
thomas <thomas.mergem@gmail.com>
2023-10-18 19:29:15 +0200

    "plus beau"

--- src/business/joueur/get_info_party.py
+++ src/business/joueur/get_info_party.py
@@ -6,36 +6,52 @@ import json
 with open('src/business/joueur/infos_partie.json', 'r') as fichier_json:
     infos_partie = json.load(fichier_json)
     
-info = infos_partie.get("info")
+def extract_participant_info(json_data):
+    """ Obtenir les informations d'une partie à partir d'un JSON
 
-# Accéder aux informations spécifiques pour chaque participant
-participants = info.get("participants", [])
-
-# Créer un dictionnaire pour stocker les informations par puuid
-participant_info = {}
-
-for participant in participants:
-    puuid = participant["puuid"]
-    # Créer une liste avec les informations spécifiques pour ce participant
-    participant_data = [
-        info["gameDuration"],
-        participant["assists"],
-        participant["championId"],
-        participant["champLevel"],
-        participant["deaths"],
-        participant["goldEarned"],
-        participant["kills"],
-        participant["neutralMinionsKilled"],
-        participant["role"],
-        participant["lane"],
-        participant["summoner1Id"],
-        participant["summoner2Id"],
-        participant["totalDamageDealt"],
-        participant["win"],
-        participant["teamId"]
-    ]
+    Parameters :
     
-    # Assigner la liste d'informations au puuid dans le dictionnaire
-    participant_info[puuid] = participant_data
+        json_data : JSON 
 
-print(participant_info)
+    Return :
+
+        None
+    """
+    info = json_data.get("info")
+
+    # Accéder aux informations spécifiques pour chaque participant
+    participants = info.get("participants", [])
+
+    # Créer une liste pour stocker les informations de tous les participants
+    participant_info = []
+    participant_info.append(info["gameDuration"])
+
+    for participant in participants:
+        # Créer une liste avec les informations spécifiques pour ce participant
+        participant_data = [
+            participant["puuid"],
+            participant["assists"],
+            participant["championId"],
+            participant["champLevel"],
+            participant["deaths"],
+            participant["goldEarned"],
+            participant["kills"],
+            participant["neutralMinionsKilled"],
+            participant["role"],
+            participant["lane"],
+            participant["summoner1Id"],
+            participant["summoner2Id"],
+            participant["totalDamageDealt"],
+            participant["win"],
+            participant["teamId"]
+        ]
+
+        # Étendre la liste d'informations à la liste des participants
+        participant_info.extend(participant_data)
+
+    return participant_info
+    
+""" Voir le résultat sur un exemple
+test = extract_participant_info(infos_partie)
+print(test)
+"""
