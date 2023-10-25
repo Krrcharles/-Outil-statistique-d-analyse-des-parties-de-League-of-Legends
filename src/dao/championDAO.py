4ed47da395e681154adfb1aa60c5a16c329b7d56
Sidoine <sidoine.djuffo-kuete@eleve.ensai.fr>
2023-10-25 14:30:42 +0200

    jfghdg

--- src/dao/championDAO.py
+++ src/dao/championDAO.py
@@ -1,38 +1,38 @@
+import psycopg2
 import sqlite3
 
-class ChampionDAO:
+class championDAO:
+    
     def __init__(self, db_file):
         self.conn = sqlite3.connect(db_file)
         self.cursor = self.conn.cursor()
 
-        # Étape 2 : Exécuter une requête SQL pour ajouter la nouvelle colonne
-        games_played = 'sum_column'
-        self.cursor.execute('ALTER TABLE db_file ADD COLUMN {} INTEGER'.format(games_played))
-        self.conn.commit()
-
-    # Étape 3 : Mettre à jour les enregistrements existants avec la somme des deux autres colonnes
-    def update_conn_with_sum_column(self):
-        self.cursor.execute('UPDATE conn SET {} = wins + losses'.format(games_played))
-        self.conn.commit()
-
     # Afficher la liste des champions suivant le nombre de games joués
     def get_conn_by_games_played(self):
-        self.cursor.execute('SELECT * FROM conn ORDER BY {}'.format(games_played))
-        return self.cursor.fetchall()
+        request='SELECT * '
+                ' FROM conn '
+                ' ORDER BY {}'.format(wins+losses)
 
+    # Etape 1 : On récupère une connexion en utilisant la classe DBConnection.
+    with DBConnection().connection as connection :
 
-# Exemple d'utilisation
-if __name__ == '__main':
-    db_file = 'data/database.db'
-    champion_dao = ChampionDAO(db_file)
+    # Etape 2 : à partir de la connexion on fait un curseur pour la requête 
+        with connection.cursor() as cursor : 
+        
+        # Etape 3 : on exécute notre requête SQL.
+                cursor.execute(request)
+        
+        # Etape 4 : on stocke le résultat de la requête
+                res = cursor.fetchall()
 
-    # Étape 2 : Ajouter la nouvelle colonne et mettre à jour les enregistrements existants
-    champion_dao.update_conn_with_sum_column()
+        # Étape 2 : Exécuter une requête SQL pour ajouter la nouvelle colonne
+        #games_played = 'sum_column'
+        #self.cursor.execute('ALTER TABLE db_file ADD COLUMN {} INTEGER'.format(games_played))
+        #self.conn.commit()
 
-    # Étape 3 : Récupérer la liste des champions triés par la nouvelle colonne
-    champions = champion_dao.get_conn_by_games_played()
-    for champion in champions:
-        print(champion)
+    # Étape 3 : Mettre à jour les enregistrements existants avec la somme des deux autres colonnes
+    #def update_conn_with_sum_column(self):
+     #   self.cursor.execute('UPDATE conn SET {} = wins + losses'.format(games_played))
+      #  self.conn.commit()
 
-    champion_dao.conn.close()
-
+  
