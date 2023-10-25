006a395dbf4a6d8c6f87b9231b790d87b4b6a335
thomas <thomas.mergem@gmail.com>
2023-10-25 12:14:09 +0200

    "+"

--- src/view/connection_view.py
+++ src/view/connection_view.py
@@ -9,16 +9,18 @@ class ConnectionView(AbstractView):
         self.__questions = [
             {
                 "type": "input",
-                "name": "password",
+                "Identifiant": "login",
+                "Mot de passe":"password"
                 "message": "What's your password",
             }
         ]
 
     def display_info(self):
-        print(f"Hello, please enter your password")
+        print(f"Hello, please enter your login and password")
 
     def make_choice(self):
         answers = prompt(self.__questions)
+        Session().user_name = answers["login"]
         Session().user_name = answers["password"]
 
         from view.start_view import StartView
