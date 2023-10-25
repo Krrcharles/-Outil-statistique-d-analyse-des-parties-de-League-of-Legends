9d243cc01017e3e93502f26504fb13d3a7485a76
Bamara <sorobamara7@gmail.com>
2023-10-25 15:45:40 +0200

    modified stat_player

---
+++ src/services/Stat_Champion.py
@@ -0,0 +1,13 @@
+from typing import List, Union
+
+from utils.singleton import Singleton
+
+
+
+class StatChampion(metaclass=Singleton) : 
+
+    def afficher_parties() : 
+        pass
+
+    def afficher_stat_player () : 
+        pass
---
+++ src/services/Stat_Player.py
@@ -0,0 +1,15 @@
+from typing import List, Union
+
+from utils.singleton import Singleton
+
+
+
+class StatPlayer(metaclass=Singleton) : 
+
+    def afficher_parties() : 
+        pass
+
+    def afficher_stat_player () : 
+        pass
+
+    
