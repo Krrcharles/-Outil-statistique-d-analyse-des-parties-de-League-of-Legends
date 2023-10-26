18ae3122f67c6e70c2f6f4e411d35566bb94b316
Carlos <charleswcarrere@gmail.com>
2023-10-26 19:30:40 +0200

    data repush

--- data/fill_database.py
+++ data/fill_database.py
@@ -3,13 +3,14 @@ import pandas as pd
 #RGAPI-0282fe35-e799-4f1a-b81f-713b8c4b7d7b
 api_key = input('API Key : ')
 
+#####JOUEUR#####
 challengers_url = 'https://euw1.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5'
 final_challengers_url = challengers_url + '?api_key=' + api_key
 
 challengers_data = requests.get(final_challengers_url)
 time.sleep(1.2)
 
-joueurs = pd.DataFrame(challengers_data.json()['entries'])
+joueurs = pd.DataFrame(challengers_data.json()['entries'])[:5]
 
 puuid = []
 level = []
@@ -19,19 +20,10 @@ for id_joueur in joueurs['summonerId']:
     puuid.append(summoner_data['puuid'])
     level.append(summoner_data['summonerLevel'])
     time.sleep(1.2)
-    
-puuid = []
-level = []
-for id_joueur in joueurs['summonerId']:
-    summoner_url = 'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/' + id_joueur + '?api_key=' + api_key
-    summoner_data = requests.get(summoner_url).json()
-    puuid.append(summoner_data['puuid'])
-    level.append(summoner_data['summonerLevel'])
-    time.sleep(1.2)
+    print("ca avance 1")
 
 joueurs['puuid'] = puuid
 joueurs['level'] = level
-joueurs = joueurs[:100]
 
 matches = []
 for puuid_joueur in joueurs['puuid']:
@@ -42,8 +34,30 @@ for puuid_joueur in joueurs['puuid']:
     
 joueurs['matches'] = matches
 
+#####PARTICIPANT#####
+
 # Connect to a database (or create one)
 conn = sqlite3.connect('data/database_test.db')
 joueurs['matches'] = joueurs['matches'].apply(json.dumps)
 joueurs.to_sql('joueur', conn, if_exists='replace', index=False)
 conn.close()
+
+# Connect to the SQLite database
+conn = sqlite3.connect('../data/database.db')
+
+# Read the table into a dataframe
+df = pd.read_sql('SELECT * FROM joueur', conn)
+
+# Convert the 'Scores' column back to a list
+df['matches'] = df['matches'].apply(json.loads)
+
+# Close the connection
+conn.close()
+
+# Extraire les deux premiers éléments de chaque liste
+df['matches'] = df['matches'].apply(lambda x: x[:2])
+
+# Aplatir la colonne
+liste_aplatie = df.explode('matches')['matches'].unique().tolist()
+
+
