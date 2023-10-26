052c4d7a503efb98774c5cd4b274c550744c99d6
thomas <freythomas@orange.fr>
2023-10-26 16:25:19 +0200

    change to player and player_services

--- src/services/player_service.py
+++ src/services/player_service.py
@@ -1,16 +1,20 @@
-from typing import List, Union
-
-from utils.singleton import Singleton
+from src.business.player.player import Player
 
 
+class PlayerService(): 
 
-class PlayerService(metaclass=Singleton) : 
+    #def afficher_parties(self, player: Player):
+    #    pass
 
-    def afficher_parties_from_websites() : 
-        pass
+    def afficher_stat_player(self, player):
+        """
+        """
+        # Demander nom du player à la DAO
+        P = Player('Teemo_du_666', '12', '13', 'gold', 12, 13, 178)
+        PS = PlayerService()
+        PS.afficher_stat_player(P)
+        winrate = round(player._win / (player._win + player._losses) * 100)
 
-    def afficher_stat_player_from_websites () : 
-        pass
+        affichage = f"{player._name} - Level {player._level} - {player._rank}\n\t{player._win} - {player._losses} ({winrate}%)"
 
-
-    
+        print(affichage)
