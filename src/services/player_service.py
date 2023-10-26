ec8d0d836162a394c31012047b0a0d1276528d73
thomas <freythomas@orange.fr>
2023-10-26 17:13:17 +0200

    ouai

--- src/services/player_service.py
+++ src/services/player_service.py
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 from src.business.player.player import Player
 
-=======
-from typing import List, Union
-from src.business.player import Player
-from utils.singleton import Singleton
->>>>>>> 8c26e7cb55eaadfe97d78e6778e37d9b89e4495d
 
 class PlayerService(): 
 
@@ -16,31 +10,12 @@ class PlayerService():
         """
         """
         # Demander nom du player à la DAO
+        # Demander nom du player à la DAO
         P = Player('Teemo_du_666', '12', '13', 'gold', 12, 13, 178)
         PS = PlayerService()
         PS.afficher_stat_player(P)
         winrate = round(player._win / (player._win + player._losses) * 100)
 
-<<<<<<< HEAD
         affichage = f"{player._name} - Level {player._level} - {player._rank}\n\t{player._win} - {player._losses} ({winrate}%)"
-=======
-
-    """ Il s'agit de la classe qui permet de récuperer les informations concernant un joueur """
-
-
-    def afficher_parties_from_websites(self, name) : 
-        """ 
-        Fonction qui prend le nom d'un joueur et affiche ses dernières parties
-
-        Parameters
-        ----------
-        name : nom du joueur 
-
-        Return
-        ------
-
-        """
-        
->>>>>>> 8c26e7cb55eaadfe97d78e6778e37d9b89e4495d
 
         print(affichage)
