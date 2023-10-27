3ba35df0c0f4fbf36776bf598f5f2e90e2e11797
Sidoine <sidoine.djuffo-kuete@eleve.ensai.fr>
2023-10-27 10:40:08 +0200

    gfdsh

--- src/dao/participantDAO.py
+++ src/dao/participantDAO.py
@@ -1,6 +1,6 @@
 import psycopg2
 
-from typing import Optional
+from typing import List, Optional
 from src.dao.db_connection import AbstractDAO
 from src.utils.singleton import Singleton
 
@@ -16,9 +16,8 @@ class participantDAO(metaclass=Singleton):
         :return: A list of the statistics of the participant
         :rtype: List of str
         """
-        statistics = []  # Pour stocker les statistiques
 
-        with AbstractDAO.connection().connection as connection:
+        with AbstractDAO().connection as connection:
             with connection.cursor() as cursor:
                 cursor.execute(
                         "SELECT                                          "
@@ -30,25 +29,13 @@ class participantDAO(metaclass=Singleton):
                         "       GROUP BY championName ;                  "
                         )
 
-    
-# Récupérer les résultats de la requête
-            results = cursor.fetchall()
-
+                results = cursor.fetchall()   # Récupérer les résultats de la requête
+            statistics: list[str] = []  # Pour stocker les statistiques
             # Pour chaque résultat, créer une chaîne de statistiques et l'ajouter à la liste
-            for result in results:
+        for result in results:
                 champion_name, total_parties, parties_gagnees, winrate = result
                 stat_str = f"Champion: {champion_name}, Total Parties: {total_parties}, Parties Gagnées: {parties_gagnees}, Winrate: {winrate}"
                 statistics.append(stat_str)
 
         return statistics  # Retourner la liste des statistiques
 
-if __name__ == "__main__":
-    # Créez une instance de participantDAO
-    dao = participantDAO()
-
-    # Appelez la fonction find_all_statistics pour obtenir les statistiques
-    statistics = dao.find_all_statistics()
-
-    # Affichez les statistiques
-    for stat in statistics:
-        print(stat)
