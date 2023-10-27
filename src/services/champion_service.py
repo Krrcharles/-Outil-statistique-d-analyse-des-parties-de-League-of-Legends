970e004add8448ae5a6359519119c9471c82bb56
thomas <freythomas@orange.fr>
2023-10-27 16:53:55 +0200

    avant la coupure

--- src/services/champion_service.py
+++ src/services/champion_service.py
@@ -1,20 +1,22 @@
 from typing import List, Union
-from utils.singleton import Singleton
+from src.utils.singleton import Singleton
 from src.dao.participantDAO import participantDAO
 
 
 class ChampionService(metaclass=Singleton):
     """
     """
-    def classement_champion():
+    def classement_champion(critère):
         """
         Renvoyer un classement des meilleurs champions
-        """
-        critere_affichage=["Per_game","Per_winrate","Per_KDA","Per_gold","Per_lane","Per_other_stat"]
-        classement = participantDAO().find_best_champ("Per_winrate")
-        
-        
-        
-        
-        pass
 
+        Critère = ["Per_game","Per_winrate","Per_KDA","Per_gold","Per_lane","Per_other_stat"]
+        """
+        classement = participantDAO().find_best_champ(critere)
+        
+        
+        print(classement)
+        return False
+
+# A=ChampionService
+# A.classement_champion("Per_winrate")
