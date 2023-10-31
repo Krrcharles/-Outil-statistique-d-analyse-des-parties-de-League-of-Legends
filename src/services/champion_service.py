746bb76a2aa3718b092910d97ebd21be9ab04b6a
thomas <freythomas@orange.fr>
2023-10-31 21:24:07 +0100

    champion services gold update

--- src/services/champion_service.py
+++ src/services/champion_service.py
@@ -47,8 +47,31 @@ class ChampionService(metaclass=Singleton) :
                 affichage_finale = f"{affichage_finale}\n{nouvelle_ligne}"
             affichage_finale = f"{affichage_finale}\n{separateur}"
 
+        elif critere == "Per_gold":
+            classement = participantDAO().find_champ_other()
+            affichage = "| Voici le classement des champions par gold gagnés |"
+
+            separateur = "+" + "-" * (len(affichage) - 2) + "+"
+            affichage_finale = f"{separateur}\n{affichage}\n{separateur}"
+
+            for i in range(0,9):
+                champion_info = classement[i].split(', ')
+                champion_nom = champion_info[0].split(': ')[1]
+                champion_critere = champion_info[2].split(': ')[1]
+
+                espaces_debut = (len(affichage) - len(f"| {champion_nom} : {champion_critere} gold gagnés")) // 2
+                espaces_fin = len(affichage) - len(f"| {champion_nom} : {champion_critere} gold gagnés") - espaces_debut
+                nouvelle_ligne = f"|{' ' * espaces_debut}{champion_nom} : {champion_critere} gold gagnés{' ' * espaces_fin}|"
+                affichage_finale = f"{affichage_finale}\n{nouvelle_ligne}"
+            affichage_finale = f"{affichage_finale}\n{separateur}"
+
+        else:
+            print("Mauvais critère")
+            return False
+
         print(affichage_finale)
         return False
 
 #ChampionService().classement_champion("Per_game")
 #ChampionService().classement_champion("Per_winrate")
+#ChampionService().classement_champion("Per_gold")
