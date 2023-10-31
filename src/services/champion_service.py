be45bcd70264e7f8043cace43263a51c7380849c
thomas <freythomas@orange.fr>
2023-10-31 20:26:18 +0100

    classement in champion_service +up player_services

--- src/services/champion_service.py
+++ src/services/champion_service.py
@@ -1,13 +1,54 @@
 from typing import List, Union
-
-from utils.singleton import Singleton
-
+from src.utils.singleton import Singleton
+from src.dao.participantDAO import participantDAO
 
 
 class ChampionService(metaclass=Singleton) : 
 
-    def afficher_parties() : 
-        pass
+    def classement_champion(self, critere):
+        """
+        Renvoyer un classement des meilleurs champions
+        Critère = ["Per_game","Per_winrate","Per_KDA","Per_gold","Per_lane","Per_other_stat"]
+        """
+        if critere == "Per_game":
+            classement = participantDAO().find_best_champ()
+            affichage = "| Voici le classement des champions par parties jouées |"
 
-    def afficher_stat_player () : 
-        pass
+            separateur = "+" + "-" * (len(affichage) - 2) + "+"
+            affichage_finale = f"{separateur}\n{affichage}\n{separateur}"
+
+            for i in range(0,9):
+                champion_info = classement[i].split(', ')
+                champion_nom = champion_info[0].split(': ')[1]
+                champion_critere = champion_info[1].split(': ')[1]
+
+                espaces_debut = (len(affichage) - len(f"| {champion_nom} : {champion_critere} parties jouées")) // 2
+                espaces_fin = len(affichage) - len(f"| {champion_nom} : {champion_critere} parties jouées") - espaces_debut
+                nouvelle_ligne = f"|{' ' * espaces_debut}{champion_nom} : {champion_critere} parties jouées{' ' * espaces_fin}|"
+                affichage_finale = f"{affichage_finale}\n{nouvelle_ligne}"
+
+            affichage_finale = f"{affichage_finale}\n{separateur}"
+
+        elif critere == "Per_winrate":
+            classement = participantDAO().find_champ_winrate()
+            affichage = "| Voici le classement des champions par winrate |"
+
+            separateur = "+" + "-" * (len(affichage) - 2) + "+"
+            affichage_finale = f"{separateur}\n{affichage}\n{separateur}"
+
+            for i in range(0,9):
+                champion_info = classement[i].split(', ')
+                champion_nom = champion_info[0].split(': ')[1]
+                champion_critere = champion_info[3].split(': ')[1]
+
+                espaces_debut = (len(affichage) - len(f"| {champion_nom} : {champion_critere}% de parties gagnés")) // 2
+                espaces_fin = len(affichage) - len(f"| {champion_nom} : {champion_critere}% de parties gagnés") - espaces_debut
+                nouvelle_ligne = f"|{' ' * espaces_debut}{champion_nom} : {champion_critere}% de parties gagnés{' ' * espaces_fin}|"
+                affichage_finale = f"{affichage_finale}\n{nouvelle_ligne}"
+            affichage_finale = f"{affichage_finale}\n{separateur}"
+
+        print(affichage_finale)
+        return False
+
+#ChampionService().classement_champion("Per_game")
+#ChampionService().classement_champion("Per_winrate")
--- src/services/player_service.py
+++ src/services/player_service.py
@@ -5,38 +5,58 @@ from src.dao.participantDAO import participantDAO
 class PlayerService(): 
     def afficher_parties(self, player: Player):
         """
-        
-        """
+        Affiche les informations des parties d'un joueur.
 
-        pass
+        Parameters
+        ----------
+        player : Player
+            Un objet Player représentant le joueur.
+        """
+        liste_parties = participantDAO().getpartie(player.summonerID)
+
+        for partie in liste_parties:
+            kda = (partie.kills + partie.assists) / partie.deaths
+            po = partie.goldEarned / partie.gameDuration
+
+            affichage_top = f"{partie.championName} - {partie.lane} - {partie.win}"
+            affichage_mid = f"{partie.kills}/{partie.deaths}/{partie.assists} ({kda} KDA) - {partie.totalDamageDone} dégats"
+            affichage_bot = f"{po} gold par minutes"
+
+            max_lenght = max(len(affichage_top), len(affichage_mid), len(affichage_bot))
+
+            separateur = "+" + "-" * max_lenght + "+"
+            affichage_top = "|" + affichage_top + " " * (max_lenght - len(affichage_top)) + "|"
+            affichage_mid = "|" + affichage_mid + " " * (max_lenght - len(affichage_mid)) + "|"
+            affichage_bot = "|" + affichage_bot + " " * (max_lenght - len(affichage_bot) - 6) + "|"
+
+            affichage_finale = f"{affichage_finale}\n{separateur}\n{affichage_top}\n{affichage_mid}\n{affichage_bot}\n{separateur}"
+
+        print(affichage_finale)
+        return False
 
     def afficher_stat_player(self, player):
         """
-        """
-        #P = PlayerDAO().find_player_by_name(player)
-        player_dao = PlayerDAO()
-        result = player_dao.find_player_by_name(player)
+        Affiche les statistiques d'un joueur.
 
-        winrate = round(result._win / (result._win + result._losses) * 100)
-        if result._rank == "I":
+        Parameters
+        ----------
+        player : str
+            Le nom du joueur.
+        """
+        P = PlayerDAO().find_player_by_name(player)
+
+        winrate = round(P._win / (P._win + P._losses) * 100)
+        if P._rank == "I":
             rank = "Challenger"
-        
-        cote = "|"
-        coins = "+"
-        
-        affichage_top = f"{result._name} - Level {result._level} - {rank}"
-        affichage_bot= f"\t{result._win} Victoires / {result._losses} Défaite ({winrate}%)"
+
+        affichage_top = f"{P._name} - Level {P._level} - {rank}"
+        affichage_bot= f"\t{P._win} Victoires / {P._losses} Défaite ({winrate}%)"
 
         max_lenght = max(len(affichage_top), len(affichage_bot) + 8)
-        separateur = coins + "-" * max_lenght + coins
-        affichage_top = cote + affichage_top + " " * (max_lenght - len(affichage_top)) + cote
-        affichage_bot = cote + affichage_bot + " " * (max_lenght - len(affichage_bot) - 6) + cote
+        separateur = "+" + "-" * max_lenght + "+"
+        affichage_top = "|" + affichage_top + " " * (max_lenght - len(affichage_top)) + "|"
+        affichage_bot = "|" + affichage_bot + " " * (max_lenght - len(affichage_bot) - 6) + "|"
 
-        print(separateur)
-        print(affichage_top)
-        print(affichage_bot)
-        print(separateur)
-        #affichage_finale = f"{separateur}\n{affichage_top}\n{affichage_bot}\n{separateur}"
-        #return affichage_finale
-
-PlayerService().afficher_stat_player("TwTv Raideru")
+        affichage_finale = f"{separateur}\n{affichage_top}\n{affichage_bot}\n{separateur}"
+        print(affichage_finale)
+        return False
