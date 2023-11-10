7747fb36583f8e29bf00878dfe5240a2b1db5108
Bamara <sorobamara7@gmail.com>
2023-11-10 16:59:37 +0100

    premiers tests

---
+++ src/tests/test_PlayerDAO.py
@@ -0,0 +1,7 @@
+import unittest
+from src.dao.playerDAO import PlayerDAO
+
+class TestPlayerDAO() : 
+    
+    def test_find_player_by_name_playerDAO () :
+        pass
---
+++ src/tests/test_userDAO.py
@@ -0,0 +1,13 @@
+import unittest
+from src.dao.userDAO import UserDAO
+
+class TestUserDAO() : 
+    
+    def test_rajouter_utilisateur_userDAO () :
+        pass
+
+    def test_verifier_utilisateur_userDAO () :
+        pass
+
+    def test_verifier_mdp_userDAO() :
+        pass
