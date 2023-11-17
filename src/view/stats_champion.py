31e1adea89bbba5db99c1f58c2e2d26fa1dd37dd
Carlos <charleswcarrere@gmail.com>
2023-11-17 18:06:40 +0100

    implément stat champion view WORKING

--- src/view/stats_champion.py
+++ src/view/stats_champion.py
@@ -3,7 +3,52 @@ from InquirerPy.separator import Separator
 
 from src.view.abstract_view import AbstractView
 from src.view.session import Session
-from src.view.connexion_view import ConnexionView
+from src.services.champion_service import ChampionService
 
 class StatsChampion(AbstractView):
-    #classe à définir demain
+    def __init__(self):
+        pass
+    
+    def display_info(self):
+        pass
+    
+    def make_choice(self): 
+        champ = input("Enter champion name")
+        ChampionService().stat_champion(champ)
+
+        choice = prompt(
+            [
+                {
+                    "type": "confirm",
+                    "name": "yes",
+                    "message": "Do you want to get another information about another champion ?",
+                    "default": True,
+                }
+            ]
+        )
+
+        if choice["yes"]:
+            from src.view.stats_player import StatsPlayer
+
+            return StatsChampion()
+
+        else:
+            if Session().user_identifiant != "unknown" :
+                if Session().not_admin == "no" :
+                    from src.view.admin_view import AdminView
+                    Session().user_identifiant
+
+                    return AdminView()
+                
+                else :
+                    from src.view.member_view import MemberView
+                    Session().user_identifiant
+
+                    return MemberView()
+            
+            else :
+                from src.view.invite_view import InviteView
+                
+                return InviteView()
+    
+    
