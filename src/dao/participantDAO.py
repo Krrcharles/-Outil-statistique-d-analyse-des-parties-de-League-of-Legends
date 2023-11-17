9870b9a22aa995cd26a471196990d2b7c460c30b
Charles CARRERE <43387511+Krrcharles@users.noreply.github.com>
2023-11-17 16:03:06 +0000

    fix merge

--- src/dao/participantDAO.py
+++ src/dao/participantDAO.py
@@ -160,12 +160,8 @@ print(result)
 champion_name = "Sylas"
 participant_dao = ParticipantDAO()
 result = participant_dao.stat_champ_by_name(champion_name)
-<<<<<<< HEAD
 
 
-=======
-"""
->>>>>>> 08b1ea57cfe64ffb1b9e551e82c1cfc402d538b8
 """
 particip_dao = ParticipantDAO()
 result = particip_dao.getpartie("VIVE Serendrip")
