c68c52d668b5e4381dcf796b32484306819a05e5
thomas <freythomas@orange.fr>
2023-11-19 22:36:17 +0100

    changement instance getpartie()

--- src/services/player_service.py
+++ src/services/player_service.py
@@ -22,16 +22,19 @@ class PlayerService():
             print("Ce joueur n'a pas de partie dans la base de données")
             return False
 
-        elif type(liste_parties) is tuple:
-            kda = (float(liste_parties[3]) + float(liste_parties[5])) / float(liste_parties[4])
-            if liste_parties[2] == 1:
+        affichage_finale = ""
+
+        for partie in liste_parties:
+            kda = round((float(partie._kills[0]) + float(partie._assists[0])) / float(partie._death[0]), 2)
+            gold_min = round(partie._goldEarned[0] / partie._gameDuration)
+            if partie._win[0] == 1:
                 win = "Victoire"
             else:
                 win = "Défaite"
 
-            affichage_top = f"{liste_parties[0]} - {liste_parties[1]} - {win}"
-            affichage_mid = f"{liste_parties[3]}/{liste_parties[4]}/{liste_parties[5]} ({kda} KDA) - {liste_parties[6]} dégats"
-            affichage_bot = f"{liste_parties[7]} gold par minutes"
+            affichage_top = f"{partie._championName[0]} - {partie._lane[0]} - {win}"
+            affichage_mid = f"{partie._kills[0]}/{partie._death[0]}/{partie._assists[0]} ({kda} KDA) - {partie._totalDamageDealtToChampions[0]} dégats"
+            affichage_bot = f"{gold_min} gold par minutes"
 
             max_lenght = max(len(affichage_top), len(affichage_mid), len(affichage_bot))
 
@@ -40,30 +43,7 @@ class PlayerService():
             affichage_mid = "|" + affichage_mid + " " * (max_lenght - len(affichage_mid)) + "|"
             affichage_bot = "|" + affichage_bot + " " * (max_lenght - len(affichage_bot)) + "|"
 
-            affichage_finale = f"{separateur}\n{affichage_top}\n{affichage_mid}\n{affichage_bot}\n{separateur}"
-
-        else:
-            affichage_finale = ""
-
-            for partie in liste_parties:
-                kda = round((float(partie[3]) + float(partie[5])) / float(partie[4]), 2)
-                if partie[2] == 1:
-                    win = "Victoire"
-                else:
-                    win = "Défaite"
-
-                affichage_top = f"{partie[0]} - {partie[1]} - {win}"
-                affichage_mid = f"{partie[3]}/{partie[4]}/{partie[5]} ({kda} KDA) - {partie[6]} dégats"
-                affichage_bot = f"{partie[7]} gold par minutes"
-
-                max_lenght = max(len(affichage_top), len(affichage_mid), len(affichage_bot))
-
-                separateur = "+" + "-" * max_lenght + "+"
-                affichage_top = "|" + affichage_top + " " * (max_lenght - len(affichage_top)) + "|"
-                affichage_mid = "|" + affichage_mid + " " * (max_lenght - len(affichage_mid)) + "|"
-                affichage_bot = "|" + affichage_bot + " " * (max_lenght - len(affichage_bot)) + "|"
-
-                affichage_finale = f"{affichage_finale}\n{separateur}\n{affichage_top}\n{affichage_mid}\n{affichage_bot}\n{separateur}"
+            affichage_finale = f"{affichage_finale}\n{separateur}\n{affichage_top}\n{affichage_mid}\n{affichage_bot}\n{separateur}"
 
         print(affichage_finale)
         return False
@@ -103,3 +83,4 @@ class PlayerService():
         print(affichage_finale)
         return False
 
+PlayerService().afficher_parties("VIVE Serendrip")
