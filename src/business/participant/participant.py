ca0da990836e2cf03f545a402864b92cc74f54c3
Bamara <sorobamara7@gmail.com>
2023-11-25 06:20:52 +0100

    partcipant modified

--- src/business/participant/participant.py
+++ src/business/participant/participant.py
@@ -1,10 +1,4 @@
-import sqlite3, requests, json, time
-import pandas as pd
-
-
-class Participant :
-
-
+class Participant:
 
     ####################################
     #           Constructor            #
@@ -12,49 +6,58 @@ class Participant :
 
     def __init__(
         self,
-        id_game : str,
-        puuid : str,
-        teamID : int,
-        totalDamageDealtToChampions : int,
-        win : bool,
-        lane : str,
-        role : str,
-        totalMinionsKilled : int,
-        championName : str,
-        goldEarned : int,
-        death : int,
-        assists : int,
-        kills : int,
-        gameDuration : int
-    )-> None :
+        id_game: str,
+        puuid: str,
+        team_id: int,
+        total_damage_dealt_to_champions: int,
+        win: bool,
+        lane: str,
+        role: str,
+        total_minions_killed: int,
+        champion_name: str,
+        gold_earned: int,
+        death: int,
+        assists: int,
+        kills: int,
+        game_duration: int
+    ) -> None:
 
-
-    ####################################
-    #           Attributes             #
-    ####################################
+        ####################################
+        #           Attributes             #
+        ####################################
 
         self._id_game = id_game
         self._puuid = puuid
-        self._teamID = teamID
-        self._totalDamageDealtToChampions = totalDamageDealtToChampions
+        self._team_id = team_id
+        self._total_damage_dealt_to_champions = total_damage_dealt_to_champions
         self._win = win
         self._lane = lane
         self._role = role
-        self._totaleMinionsKilled = totalMinionsKilled
-        self._championName = championName
-        self._goldEarned = goldEarned
+        self._total_minions_killed = total_minions_killed
+        self._champion_name = champion_name
+        self._gold_earned = gold_earned
         self._death = death
         self._assists = assists
         self._kills = kills
-        self._gameDuration = gameDuration
-        
+        self._game_duration = game_duration
 
     ####################################
     #             Methods              #
     ####################################
-
     def __str__(self):
-        """
-        Affichage d'un joueur
-        """
-        return f"ID: {self._id_game}, PUUID : {self._puuid}, \n win : {self._win}, totalDamageDealtToChampions :{self._totalDamageDealtToChampions}, lane : {self._lane}, \n totalMinionsKilled : {self._totaleMinionsKilled} "
+        return (
+            f"ID: {self._id_game}, "
+            f"PUUID: {self._puuid}, "
+            f"Team ID: {self._team_id}, "
+            f"Win: {self._win}, "
+            f"Total Damage Dealt to Champions: {self._total_damage_dealt_to_champions}, "
+            f"Lane: {self._lane}, "
+            f"Role: {self._role}, "
+            f"Total Minions Killed: {self._total_minions_killed}, "
+            f"Champion Name: {self._champion_name}, "
+            f"Gold Earned: {self._gold_earned}, "
+            f"Death: {self._death}, "
+            f"Assists: {self._assists}, "
+            f"Kills: {self._kills}, "
+            f"Game Duration: {self._game_duration}"
+        )
