f7a1753edf14fbbc42c1e5718e304af174dc0ac9
Charles CARRERE <43387511+Krrcharles@users.noreply.github.com>
2023-11-25 17:18:00 +0000

    cleaner code main

--- __main__.py
+++ __main__.py
@@ -7,17 +7,17 @@ from data.fill_database import fill
 # This script is the entry point of your application
 
 if __name__ == "__main__":
+
     dotenv.load_dotenv(override=True)
-    
+
     #si l'utilisateur lance l'application pour la première fois
     #il n'a pas de DB de constitué et donc il faut l'initaliser
-    #pour le moment j'admet que ca marche psk ca marche quand je teste a la main
     if not os.path.exists(os.path.exists(os.path.join(os.getcwd(),'databse.db'))):
         print('Base de données non trouvée,\n initialisation ...')
         creer_database()
         fill()
-        
-    
+  
+
     # run the Start View
     current_view = StartView()
 
